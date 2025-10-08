06d3f22444f42636c4059c48fb00a3d99bd6b997
robert.main@mcgill.ca <robert.main@mcgill.ca>
2025-10-08 16:02:33 -0400

    Clean FFA branch commit

---
+++ champss/FFA/multiday_search.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LINE_PROFILE=1 python FFA_search.py --psr "B0154+61" --date 20240602 --generate_candidates -s
+# python FFA_search.py --psr "B0154+61" --date 20240603 --generate_candidates -s
+# python FFA_search.py --psr "B0154+61" --date 20240604 --generate_candidates -s
+# python FFA_search.py --psr "B0154+61" --date 20240605 --generate_candidates -s
+# python FFA_search.py --psr "B0154+61" --date 20240606 --generate_candidates -s
+# python FFA_search.py --psr "B0154+61" --date 20240607 --generate_candidates -s
+# python FFA_search.py --psr "B0154+61" --date 20240608 --generate_candidates -s
+# python FFA_search.py --psr "B0154+61" --date 20240609 --generate_candidates -s
+# python FFA_search.py --psr "B0154+61" --date 20240610 --generate_candidates
+
+# python FFA_search.py --ra 160 --dec 50 --date 20240610 --generate_candidates -s --plot
+# python FFA_search.py --ra 286.34 --dec 6.18 --date 20240610 --plot --no_candidates
+# python FFA_search.py --ra 312.44392352319824 --dec 50.790955248269036 --date 20240610 --plot 
+
+# Sigma jumped pointings
+# python FFA_search.py --ra 38.94979604018291 --dec 50.77917855620054 --date 20240610 --plot --generate_candidates
+# python FFA_search.py --ra 22.382646040747534 --dec 49.98624078822707 --date 20240610 --plot --generate_candidates
